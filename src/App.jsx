17ec58329ff2f3e0de1f110786c1c1af9141ce31
olcayaltun <155670863+olcayaltun@users.noreply.github.com>
2025-02-20 10:15:00 +0100

    fiüösit

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./pages/Main";
 import Test from "./pages/Test";
 import TrenB from "./pages/Tren";
-import { DndProvider } from "react-dnd";
-import { HTML5Backend } from "react-dnd-html5-backend"; // HTML5 backend'i k
+
 const App = () => {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/test" element={<Test />} />
-          <Route path="/tren" element={<TrenB />} />
-        </Routes>
-      </BrowserRouter>
-    </DndProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />} />
+        <Route path="/test" element={<Test />} />
+        <Route path="/tren" element={<TrenB />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
